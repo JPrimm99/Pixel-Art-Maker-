772dbb0da915cf53fc0ee996d5875dba7d634c20
Jai Primm <77127759+JPrimm99@users.noreply.github.com>
2022-07-08 11:52:00 -0500

    Add files via upload

---
+++ pixel art marker/design.js
@@ -0,0 +1,39 @@
+
+const $colorPicker = $('#colorPicker');
+const $tableElement = $('#pixelCanvas');
+const $inputHeight = $('#inputHeight');
+const $inputWidth = $('#inputWidth');
+
+
+$('#sizePicker').submit( event => {
+    event.preventDefault();
+
+    let width = $inputWidth.val();
+    let height = $inputHeight.val();
+
+    $tableElement.html(''); //clear
+
+    makeGrid(height, width);
+    addCellClickListener();
+});
+
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+        $tableElement.append('<tr></tr>');
+    };
+
+    for(let i = 0; i < width; i++) {
+        $('tr').append('<td></td>');
+    };
+};
+
+function addCellClickListener() {
+    $('td').click( event => {
+        let color = $colorPicker.val();
+        $(event.currentTarget).css("background-color", color)
+    });
+};
+    
+
+   
+    
